b963614914044e70402c6f15b4e716fde11ebc5e
Pedro Pessoa <pessoa.pedro91@gmail.com>
2019-02-07 00:08:31 -0200

    Add menu item and settings activity

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,4 +1,19 @@
 package com.example.android.quakereport;
 
-public class SettingsActivity {
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+
+    }
 }
