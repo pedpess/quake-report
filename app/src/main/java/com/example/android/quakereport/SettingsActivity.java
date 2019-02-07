1c2bfa0ad181a770af584ef7a77a99c0fbc35692
Pedro Pessoa <pessoa.pedro91@gmail.com>
2019-02-07 00:42:14 -0200

    Preference summary

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.quakereport;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,11 +16,31 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings_activity);
     }
 
-    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+    public static class EarthquakePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMagnitude);
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+        }
+
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object objectValue) {
+            String stringValue = objectValue.toString();
+
+            preference.setSummary(stringValue);
+            return true;
         }
     }
 }
